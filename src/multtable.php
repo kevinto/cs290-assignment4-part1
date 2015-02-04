a2d9bf381d7e9fedb0d763397060d7369a427d9a
U-OMNICELL\kevint <kevint@OMCL764-0HHLFAH.omnicell.com>
2015-02-03 19:06:35 -0800

    finished get param validation on multtable.

--- src/multtable.php
+++ src/multtable.php
@@ -10,33 +10,81 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-if (!isset($_GET['min-multiplicand']) || !isset($_GET['max-multiplicand']) || !isset($_GET['min-multiplier']) || !isset($_GET['max-multiplier'])) {
-  echo "<p>You are missing the following GET parameter(s):";
-
-  if (!isset($_GET['min-multiplicand'])) {
-    echo "<br>min-multiplicand";
-  }
-   
-  if (!isset($_GET['max-multiplicand'])) {
-    echo "<br>max-multiplicand";
-  }
-
-  if (!isset($_GET['min-multiplier'])) {
-    echo "<br>min-multiplier";
-  }
-
-  if (!isset($_GET['max-multiplier'])) {
-    echo "<br>max-multiplier";
-  }
-
-  // $minMultiplicand = $_GET['min-multiplicand'];
-
-  // echo "<p>The first param(min-multiplicand) value is: $minMultiplicand. \n</p>";
-  // echo "<p>The first param(min-multiplicand) value is: $minMultiplicand. \n</p>";
-
-  echo "</p>";
+if (!allGetParamsExist() || !paramsValidIntegers() || !minMaxValid()) {
+  exit();
 }
 
+createMultTable();
+
+echo "test me";
+
+function allGetParamsExist() {
+  if (!isset($_GET['min-multiplicand']) || !isset($_GET['max-multiplicand']) || !isset($_GET['min-multiplier']) || !isset($_GET['max-multiplier'])) {
+
+    if (!isset($_GET['min-multiplicand'])) {
+      echo "<p>Missing parameter [min-multiplicand].</p>";
+    }
+     
+    if (!isset($_GET['max-multiplicand'])) {
+      echo "<p>Missing parameter [max-multiplicand].</p>";
+    }
+
+    if (!isset($_GET['min-multiplier'])) {
+      echo "<p>Missing parameter [min-multiplier].</p>";
+    }
+
+    if (!isset($_GET['max-multiplier'])) {
+      echo "<p>Missing parameter [max-multiplier].</p>";
+    }
+
+    // $minMultiplicand = $_GET['min-multiplicand'];
+
+    // echo "<p>The first param(min-multiplicand) value is: $minMultiplicand. \n</p>";
+    // echo "<p>The first param(min-multiplicand) value is: $minMultiplicand. \n</p>";
+    return false;
+  }
+
+  return true;
+}
+
+function minMaxValid() {
+  if ($_GET['min-multiplicand'] > $_GET['max-multiplicand'] ||$_GET['min-multiplier'] > $_GET['max-multiplier']) {
+    echo "<p>Minimum [multiplicand|multiplier] larger than maximum.</p>";
+    return false;
+  }
+
+  return true;
+}
+
+function paramsValidIntegers() {
+  $isValid = true;
+
+  if (!ctype_digit($_GET['min-multiplicand'])) {
+    echo "<p>[min-multiplicand] must be an integer.</p>";
+    $isValid = false;
+  }
+ 
+  if (!ctype_digit($_GET['max-multiplicand'])) {
+    echo "<p>[max-multiplicand] must be an integer.</p>";
+    $isValid = false;
+  } 
+    
+  if (!ctype_digit($_GET['min-multiplier'])) {
+    echo "<p>[min-multiplier] must be an integer.</p>";
+    $isValid = false;
+  }
+
+  if (!ctype_digit($_GET['max-multiplier'])) {
+    echo "<p>[max-multiplier] must be an integer.</p>";
+    $isValid = false;
+  }
+
+  return $isValid;
+}
+
+function createMultTable() {
+
+}
 ?>
 </body>
 </html>
